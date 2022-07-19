15f7e8b11ce20ea1f41f522f7e2abfdfe64424da
Kathy Campos <kathy.campos.cabello@gmail.com>
2022-07-19 12:43:45 -0500

    Primer escenario completo

--- base-continuous-testing/src/test/java/com/everis/base/stepDefinitions/PlaceHolderSD.java
+++ base-continuous-testing/src/test/java/com/everis/base/stepDefinitions/PlaceHolderSD.java
@@ -1,23 +1,34 @@
 package com.everis.base.stepDefinitions;
 
+import com.everis.base.steps.NetflixSteps;
+import com.everis.base.steps.PlaceHolderSteps;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
 
 public class PlaceHolderSD {
 
+    @Steps
+    public PlaceHolderSteps placeHolder;
+
     @Given("que la aplicación está operativa")
     public void que_la_aplicación_está_operativa() {
+        System.out.println("--------------El servicio está operativo---------");
     }
 
     @When("consulto la lista de artículos")
     public void consulto_la_lista_de_artículos() {
+        System.out.println("La lista de artículos ha sido consultada");
+        placeHolder.listPosts();
     }
     @Then("se valida que el código es {int}")
-    public void se_valida_que_el_código_es(Integer int1) {
+    public void se_valida_que_el_código_es(int i) {
+        placeHolder.validateStatusCode(i);
     }
     @And("se me mostrará en consola la lista de artículos existentes")
     public void se_me_mostrará_en_consola_la_lista_de_artículos_existentes() {
+        placeHolder.printList();
     }
 }
--- base-continuous-testing/src/test/java/com/everis/base/steps/PlaceHolderSteps.java
+++ base-continuous-testing/src/test/java/com/everis/base/steps/PlaceHolderSteps.java
@@ -1,4 +1,69 @@
 package com.everis.base.steps;
 
+import io.cucumber.java.Before;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import net.thucydides.core.annotations.Step;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static net.serenitybdd.rest.SerenityRest.given;
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class PlaceHolderSteps {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PlaceHolderSteps.class);
+    static private final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+    private static RequestSpecification requestSpec;
+    private static ResponseSpecification responseSpec;
+//    private Response response;
+    private RequestSpecBuilder builder;
+    private RequestSpecification requestSpecification;
+    private String bodyPost;
+
+    Response response = RestAssured.get(BASE_URL + "posts");
+
+    @Before
+    public void init() {
+
+        LOGGER.info(" Inicializa el constructor request ");
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri(BASE_URL)
+                .build();
+
+        LOGGER.info(" Inicializa el constructor response ");
+        responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    @Step("obtiene lista de artículos")
+    public void listPosts() {
+
+        given().
+                spec(requestSpec).
+                when().
+                get("posts").
+                then().
+                spec(responseSpec).
+                and();
+    }
+
+    public void validateStatusCode(int i) {
+        assertThat(lastResponse().statusCode(), is(i));
+    }
+    @Step("imprime la lista de artículos en consola")
+    public void printList() {
+
+        System.out.println("----------SE ESTA IMPRIMIENDO LA LISTA EN CONSOLA-------------");
+        System.out.println("Body ordenado: " + response.getBody().asPrettyString());
+    }
+
 }
